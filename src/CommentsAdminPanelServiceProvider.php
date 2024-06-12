3d3fc54169e8be97c80bc175ad2e10feba5f6178
Lakshan Madushanka <epmadushanka@gmail.com>
2024-06-12 16:24:25 +0530

    change assets group to public

--- src/CommentsAdminPanelServiceProvider.php
+++ src/CommentsAdminPanelServiceProvider.php
@@ -73,7 +73,7 @@ class CommentsAdminPanelServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__ . '/../public/vendor/lakm/comments-admin-panel' => public_path('vendor/lakm/comments-admin-panel')
-        ], 'comments-admin-panel-assets');
+        ], 'public');
 
         $this->publishes([
             __DIR__ . '/../resources' => public_path('vendor/comments-admin-panel')
